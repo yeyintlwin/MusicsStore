3ad4539c6e41057125b68040d1489a9aff606f26
yeyintlwin <yeyintlwin73@gmail.com>
2019-06-13 00:00:29 +0630

    Update Splash

--- app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SplashActivity.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SplashActivity.java
@@ -1,15 +1,34 @@
 package com.yeyintlwin.musicsstore.ui.activity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.yeyintlwin.musicsstore.ui.base.BaseActivity;
 
 @SuppressLint("Registered")
 public class SplashActivity extends BaseActivity {
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView textView=new TextView(this);
+        textView.setText("Splash");
+        setContentView(textView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,SetupActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                overridePendingTransition(0,0);
+                overridePendingTransition(0,0);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
     }
 }
