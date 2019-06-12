902de09db91333a082cb92564e0d2265d2e29d3d
yeyintlwin <yeyintlwin73@gmail.com>
2019-06-12 23:44:50 +0630

    fix bug

--- app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/MainActivity.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/MainActivity.java
@@ -1,22 +1,22 @@
 package com.yeyintlwin.musicsstore.ui.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.yeyintlwin.musicsstore.R;
+import com.yeyintlwin.musicsstore.ui.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
---
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SetupActivity.java
@@ -0,0 +1,13 @@
+package com.yeyintlwin.musicsstore.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.yeyintlwin.musicsstore.ui.base.BaseActivity;
+
+public class SetupActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
