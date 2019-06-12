2018f84c1168df63b87bf648bf8e6616b2966904
yeyintlwin <yeyintlwin73@gmail.com>
2019-06-12 22:51:49 +0630

    Create BaseActivity and SplashActivity

---
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.yeyintlwin.musicsstore.ui.activity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.yeyintlwin.musicsstore.ui.base.BaseActivity;
+
+@SuppressLint("Registered")
+public class SplashActivity extends BaseActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
