04056ea68883f71de9377983a1cb202285fcbc94
yeyintlwin <yeyintlwin73@gmail.com>
2019-06-14 19:40:56 +0630

    Designed Setup Screen

--- app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SetupActivity.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/SetupActivity.java
@@ -3,11 +3,13 @@ package com.yeyintlwin.musicsstore.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.yeyintlwin.musicsstore.R;
 import com.yeyintlwin.musicsstore.ui.activity.base.BaseActivity;
 
 public class SetupActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
     }
 }
