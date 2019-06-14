ea02831a609a437ba9e0cec2e552a7114a429005
yeyintlwin <yeyintlwin73@gmail.com>
2019-06-15 01:11:41 +0630

    Added my custom themes and colors.

--- app/src/main/java/com/yeyintlwin/musicsstore/Constants.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/Constants.java
@@ -2,4 +2,6 @@ package com.yeyintlwin.musicsstore;
 
 public class Constants {
     public static final String IS_UNICODE="isUnicode";
+    public static final String THEME="theme";
+    public static final int DEFAULT_THEME=R.style.SweetPink;
 }
--- app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/base/BaseActivity.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/activity/base/BaseActivity.java
@@ -1,12 +1,18 @@
 package com.yeyintlwin.musicsstore.ui.activity.base;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.yeyintlwin.musicsstore.Constants;
+import com.yeyintlwin.musicsstore.MainController;
+
+@SuppressLint("Registered")
 public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(MainController.getInt(Constants.THEME,Constants.DEFAULT_THEME));
     }
 }
