3a385d35014242c7bf380387b1583687b286cca3
yeyintlwin <yeyintlwin73@gmail.com>
2019-07-22 21:35:48 +0630

    Designing Control Structure.

--- app/src/main/java/com/yeyintlwin/musicsstore/ui/fragment/category/adapter/viewholder/CategoryItemViewHolder.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/fragment/category/adapter/viewholder/CategoryItemViewHolder.java
@@ -3,9 +3,24 @@ package com.yeyintlwin.musicsstore.ui.fragment.category.adapter.viewholder;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import com.yeyintlwin.musicsstore.ui.fragment.category.adapter.viewholder.listener.OnCategoryItemClickListener;
+import com.yeyintlwin.musicsstore.ui.fragment.category.entity.CategoryInfo;
 
 public class CategoryItemViewHolder extends RecyclerView.ViewHolder {
+    public CategoryInfo categoryInfo;
+    private OnCategoryItemClickListener listener;
+
     public CategoryItemViewHolder(@NonNull View itemView) {
         super(itemView);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null) listener.onCategoryItemClick(categoryInfo);
+            }
+        });
+    }
+
+    public void setOnCategoryItemClickListener(OnCategoryItemClickListener categoryItemClickListener) {
+        listener = categoryItemClickListener;
     }
 }
---
+++ app/src/main/java/com/yeyintlwin/musicsstore/ui/fragment/category/adapter/viewholder/listener/OnCategoryItemClickListener.java
@@ -0,0 +1,7 @@
+package com.yeyintlwin.musicsstore.ui.fragment.category.adapter.viewholder.listener;
+
+import com.yeyintlwin.musicsstore.ui.fragment.category.entity.CategoryInfo;
+
+public interface OnCategoryItemClickListener {
+    void onCategoryItemClick(CategoryInfo categoryInfo);
+}
