6fe1d2a274ede3606109dc5e76b185b3385c39ff
Ye Yint Lwin <yeyintlwin73gmail.com>
2020-02-20 13:44:37 +0630

    Changed Server IP Address in "Constant.java" file.

--- app/src/main/java/com/yeyintlwin/musicsstore/Constants.java
+++ app/src/main/java/com/yeyintlwin/musicsstore/Constants.java
@@ -2,5 +2,5 @@ package com.yeyintlwin.musicsstore;
 
 public class Constants {
     public static final String IS_UNICODE = "isUnicode";
-    public static final String API_V2 = "http://192.168.43.138/android_api_v2.0/user/";
+    public static final String API_V2 = "http://192.168.43.139/android_api_v2.0/user/";
 }
